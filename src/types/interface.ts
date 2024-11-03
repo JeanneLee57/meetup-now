c127b101c5161b1826e3a2b95f5b91a99809eb63
JeanneLee57 <jinhwa.lee.57@gmail.com>
2024-11-03 15:01:06 +0900

    feat: EventData 인터페이스 생성

---
+++ src/types/interface.ts
@@ -0,0 +1,9 @@
+export interface EventData {
+  title: string;
+  datetime: string;
+  location: string;
+  organizer: string;
+  description: string;
+  is_public: boolean;
+  image_url?: string;
+}
