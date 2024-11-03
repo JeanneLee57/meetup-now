65fc693c2a64043681b91ed3fa03b5ac360e8746
JeanneLee57 <jinhwa.lee.57@gmail.com>
2024-11-03 15:02:07 +0900

    feat: 이벤트 생성 폼에 supabase client 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
+import { createClient } from '@supabase/supabase-js';
+import { EventData } from '@/types/interface';
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseServer = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
+
 export default function Home() {
+  async function createEvent(formData: FormData) {
+    'use server';
+
+    const eventData: EventData = {
+      title: formData.get('title') as string,
+      datetime: formData.get('datetime') as string,
+      location: formData.get('location') as string,
+      organizer: formData.get('organizer') as string,
+      description: formData.get('description') as string,
+      is_public: formData.get('visibility') === 'public',
+    };
+
+    try {
+      const { error } = await supabaseServer.from('events').insert([eventData]);
+      if (error) throw error;
+    } catch (error) {
+      console.error('이벤트 생성 중 오류 발생:', error);
+      throw error;
+    }
+  }
+
   return (
     <div>
       <main className="p-4">
         <h1 className="text-center mb-6">새 이벤트를 계획해 보세요!</h1>
-        <form className="max-w-2xl mx-auto p-6 space-y-6">
+        <form action={createEvent} className="max-w-2xl mx-auto p-6 space-y-6">
           <div>
             <label
               className="block text-sm font-medium text-gray-700"
@@ -15,6 +48,7 @@ export default function Home() {
               required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
               id="title"
+              name="title"
               type="text"
             />
           </div>
@@ -23,13 +57,12 @@ export default function Home() {
             <label
               className="block text-sm font-medium text-gray-700"
               htmlFor="datetime"
-            >
-              날짜와 시간
-            </label>
+            ></label>
             <input
               required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
               id="datetime"
+              name="datetime"
               type="datetime-local"
             />
           </div>
@@ -45,6 +78,7 @@ export default function Home() {
               required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
               id="location"
+              name="location"
               type="text"
             />
           </div>
@@ -60,6 +94,7 @@ export default function Home() {
               required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
               id="organizer"
+              name="organizer"
               type="text"
             />
           </div>
