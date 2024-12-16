ba04f4fb047be997c12007e98f616ab8453efcf8
JeanneLee57 <jinhwa.lee.57@gmail.com>
2024-12-16 14:09:37 +0900

    feat: 이벤트 생성 시 위경도값 저장

--- src/actions/eventActions.ts
+++ src/actions/eventActions.ts
@@ -5,7 +5,21 @@ import { EventData } from '@/types/interface';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { s3Client } from '@/utils/s3';
 
+const getEventlatLng = async (address: string) => {
+  const latLng = { latitude: 0, longitude: 0 };
+  const geocoder = new window.kakao.maps.services.Geocoder();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  geocoder.addressSearch(address, (result: any, status: any) => {
+    if (status === window.kakao.maps.services.Status.OK) {
+      latLng.latitude = result[0].y;
+      latLng.longitude = result[0].x;
+    }
+  });
+  return latLng;
+};
+
 export async function createEvent(formData: FormData) {
+  const latLng = await getEventlatLng(formData.get('address') as string);
   const eventData: EventData = {
     title: formData.get('title') as string,
     datetime: formData.get('datetime') as string,
@@ -14,6 +28,8 @@ export async function createEvent(formData: FormData) {
     organizer: formData.get('organizer') as string,
     description: formData.get('description') as string,
     is_public: formData.get('visibility') === 'public',
+    latitude: latLng.latitude,
+    longitude: latLng.longitude,
   };
 
   try {
